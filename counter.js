e665013eaf3ea409ac344980e9c9d7651c25a50c
rahmotulla <75873454+rahmotulla@users.noreply.github.com>
2026-07-22 15:11:16 +0600

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,33 @@
+```javascript
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target = +counter.getAttribute("data-target");
+
+    const current = +counter.innerText;
+
+    const increment = target / 100;
+
+    if(current < target){
+
+      counter.innerText =
+      Math.ceil(current + increment);
+
+      setTimeout(updateCounter,20);
+
+    }else{
+
+      counter.innerText =
+      target.toLocaleString() + "+";
+
+    }
+
+  };
+
+  updateCounter();
+
+});
+```
